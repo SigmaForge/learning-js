f676acc350d0328a68dded4061fecc1cdc4916a2
Brice Fernandes <brice@sigmaforge.com>
2016-02-09 10:47:08 +0000

    initial exercise set

---
+++ 01-values-and-operators.js
@@ -0,0 +1,111 @@
+var assert = require('assert');
+
+var _ = undefined;
+
+/*
+ * To complete the exercise, replace underscores in the 
+ * code below with appropriate values to make the tests 
+ * pass.
+ * 
+ * See README.md for installing the test library and 
+ * running the tests.
+ */
+
+describe("Numbers", function(){
+
+	it("can be added together", function(){
+		var val = 34 + _ ;
+		assert.strictEqual( val, 40);
+	});
+
+	it("can be defined using scientific notation", function(){
+		assert.strictEqual(3e8 * 3e8,  _);
+	});
+
+	it("are floating point", function(){
+		assert.strictEqual(1/2, _);
+	});
+
+	it("will turn into NaN under certain conditions", function(){
+		assert(isNaN( 1.2 ));
+	});
+
+	it("that are NaN are not equal to themselves", function(){
+		assert.equal(NaN === NaN, _);
+	});
+
+});
+
+describe("Operators", function(){
+	it("follow the same rule of precedence as mathematics", function(){
+		assert.strictEqual( 10 * 3 + 4,  _ );
+		assert.strictEqual( 10 * (3 + 4), _ );
+	});
+
+	it("can be used to find the remainder or modulo", function(){
+		assert.strictEqual(10 % 3, _)
+	});
+
+	it("can sometimes be unary (take only one argument)", function(){
+		assert.strictEqual(typeof 1.2, _);
+		assert.strictEqual(-(6+4), _);
+	});
+
+	it("can be used to compare numbers", function(){
+		assert.strictEqual(1>2, _);
+		assert.strictEqual(1<2, _);
+	});
+
+	it("can be used to compare strings", function(){
+		assert("Hello" > _);
+	});
+
+	describe("Logical operators", function(){
+		it("apply to boolean values", function(){
+			assert(true && _);
+			assert(false || _);
+			assert(!_);
+		});
+		it("will short-circuit their arguments", function(){
+			assert.strictEqual( "Hello" || "Danger", _);
+			assert.strictEqual( "Hello" && "Danger", _);
+		});
+	});
+
+    describe("The ternary operator", function(){
+    	it("changes its resulting value based on a condition", function(){
+    		assert.strictEqual(_?3:4, 3);
+    		assert.strictEqual(false?"Brie":_, "Gouda cheese");
+    		assert.strictEqual(true?_:"Helicopter", "Hovercraft");
+    	});
+    });
+
+});
+
+describe("Strings", function(){
+	it("can be concatenated together", function(){
+		var greeting = "Hello, " + _;
+		assert.strictEqual(greeting, "Hello, Awesome.");
+	});
+});
+
+describe("Javascript", function(){
+	it("does really weird shit sometimes", function(){
+		// see https://www.destroyallsoftware.com/talks/wat
+		assert.strictEqual(100 * null, _);
+		assert.strictEqual("4" + 3 , _);
+		assert.strictEqual("4" - 3 , _);
+	    assert.strictEqual("4" * 3 , _);
+		assert.strictEqual(false == 0, _);
+	});
+
+	it("gives the special '===' comparison operator", function(){
+		assert.strictEqual(0 == false, _);
+		assert.strictEqual("" == false, _);
+		assert.strictEqual(0 === false, _);
+		assert.strictEqual("" === false, _);
+	});
+})
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # learning-js
+
+## Setting up a local test environment
+
+1. Download and install [Node.js](https://nodejs.org/)
+2. Run the command `npm --version` in the terminal and make sure it does return any errors. It should print out your version of Node's Package manager. For example:
+        ➤ npm --version
+        3.3.12
+3. Run the command `npm install -g mocha` to install the test library.
+4. Check that mocha is installed properly by running `mocha --version`, which should print out the version of mocha that you have just installed.
+
+## Running the tests.
+
+1. In the terminal, navigate to the folder containing this file on your computer.
+2. Run the command `mocha ./`. You should see something like the following:
+
+![](./resources/mocha-output.png)
+
+You can also let Mocha run tests for you automatically when your files change by running the `mocha -w ./` command.
+
+
